7f7037ce0f781a9b26f4425e4e2b3339a76a3812
at <at@at-desktop.(none)>
2009-12-10 14:18:56 +0100

    Folibas zugchanges

--- Arduino/applet/lausche.cpp
+++ Arduino/applet/lausche.cpp
@@ -1,31 +1,122 @@
+#define  tweetSize  200            //  how many chars are in one tweet
+#define  baud       9600           //  baud for serial connection
+#define beatCount   3*tweetSize    //  this defines the music array: tone, octave, duration, tone, octave, duration, tone, octave, duration
+
 #include "WProgram.h"
 void setup();
 void loop();
-int baud = 9600;
-char incomingByte = 0;
-char incomingChar[200];
-int counter = 0;
+char getTweet();
+void parseTweet(char tweet[tweetSize], int i);
+void tweet2tone();
+int generateTone(int tone, int octave);
+void playTone(int tone, int duration);
+int   speakerPin    =  13;
+char  incomingByte  =  0;
+char  incomingChar[tweetSize];
+int   counter       =  0;
+
+char  beat[beatCount];
+
 
 void setup()
 {
   Serial.begin(baud);
+  for (int i = 0; i < beatCount; i++)
+  {
+    //  fill the melody array with zeros --> nothing will be played
+    beat[i] = 0;
+  }
 }
+
 
 void loop()
 {
-  
-  if (Serial.available() > 0)
+  boolean newTweet = false;
+  while (Serial.available() > 0)
   {
+    //  get one complete tweet once in a time
     counter++;
-    incomingChar[counter] = Serial.read();    
+    incomingChar[counter] = getTweet();
+    newTweet = true;
   }
   
-  for (int i = 0; i <= counter; i++)
+  if (newTweet)
   {
-    Serial.print("read: ");
-    Serial.println(incomingChar[i]);
+    //  we have a new tweet, jeah jeah play play
+    for (int i = 0; i < tweetSize; i++)
+    {
+      parseTweet(incomingChar, i);
+    }
+  
+    //  make the music stuff
+    tweet2tone();
+  
+    //  reset counter for the next tweet
+    counter = 0;
   }
 }
+
+
+//  catch one char from from the serial port
+char getTweet()
+{
+  //  maybe we have to cache some tweets here?
+  //  maybe write a own short protocoll here?
+  char tweetChar =  Serial.read();
+
+  return tweetChar;
+}
+
+
+//  wicked switch case to parse the tweet
+void parseTweet(char tweet[tweetSize], int i)
+{
+  switch(tweet[i])
+  {
+    case 1:
+    //  normal letter/number
+    //  just a dummy to show what needs to be done here!
+      char tone    = int("a");
+      int octave   = 3;
+      int duration = 1;
+      beat[i*3]    =  tone;
+      beat[i*3+1]  =  octave;
+      beat[i*3+2]  =  duration;
+    break;
+  }
+
+}
+
+//  let's make some music for each entry in beat[]
+void tweet2tone()
+{
+  //  start the music now
+  for (int i = 0; i < beatCount; i+=3)
+  {
+    int tone = generateTone(beat[i], beat[i+1]);
+    playTone(tone, beat[i+2]);
+  }
+}
+
+
+//  merges tone + octave
+int generateTone(int tone, int octave)
+{
+  //  TODO:  merge tone with octave
+  return 1136;
+}
+
+
+//  make a beep
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
 int main(void)
 {
 	init();
---
+++ Processing/tweet2tone.pde
@@ -0,0 +1,83 @@
+import processing.serial.*;
+
+int       baud          =    9600;
+String    twitterUsr    =    "opentu";
+String    twitterPass   =    "derdepadde";
+int       sleepTime     =    60000;            //    wait 60 sec before looking for a new tweet
+int       fps           =    1;
+Twitter myTwitter;
+Serial myPort;
+
+String    lastTweet;
+
+
+void setup() {
+    frameRate(fps);
+    //println(Serial.list());
+    lastTweet = "";
+};
+
+
+void draw()
+{
+    //    catch tweet and send tweet to Arduino
+    String ardMsg = catchTweets();
+    println(ardMsg);
+    //    if this tweet differs to the last one --> send it to Arduino
+    if (ardMsg != lastTweet)
+    {
+        send2ard(ardMsg);
+        lastTweet = ardMsg;
+        
+        //    wait a while
+        delay(sleepTime);
+    }
+}
+
+
+String catchTweets()
+{
+    myTwitter     =    new Twitter(twitterUsr, twitterPass);
+    myPort        =    new Serial(this, Serial.list()[0], baud);
+    String ardMsg = " ";
+    
+    //    TODO:    documentation
+    Query query = new Query("#hs");
+    
+    try {
+        //    get just the last tweet
+        query.setRpp(1);
+        QueryResult result = myTwitter.search(query);
+        //    after sending the search query it crashes when you don't have an internet connection... that SHOULD be improved!!! :)
+        ArrayList tweets = (ArrayList) result.getTweets();
+        Tweet t = (Tweet) tweets.get(0);
+        
+        //    start buildung the string we want to send to the little arduino boy
+        String user = t.getFromUser();
+        String msg = t.getText();
+        Date tweeteDate = t.getCreatedAt();
+        println("Tweet by " + user + " at " + tweeteDate + ": " + msg);
+        ardMsg = user + "   :" + tweeteDate + ":" + msg;
+     }
+     catch (TwitterException te)
+     {
+         println("Couldn't connect: " + te);
+     };
+     
+     return ardMsg;
+}
+
+
+void send2ard(String msg)
+{
+    for (int i = 0; i < msg.length(); i++)
+    {
+          try
+          {
+              myPort.write(msg.charAt(i));
+          }
+          catch (Exception ae) {
+              println("Couldn't send to Arduino: " + ae);
+          }
+      }
+}
